485a4968f9da6d9c6796e53919be0d2e44328dc5
Erin.Schneider <erinsuzy@gmail.com>
2023-07-09 18:17:27 -0500

    updated movies chores books

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "New Releases";
+   let book1 = "https://nypost.com/wp-content/uploads/sites/2/2020/09/the-guest-room.jpg?resize=1018,1536&quality=75&strip=all";
+   let book2 = "https://nypost.com/wp-content/uploads/sites/2/2020/09/the-manor-house.jpg?resize=1022,1536&quality=75&strip=all";
+   let book3 = "https://nypost.com/wp-content/uploads/sites/2/2020/09/a-likeable-woman.jpg?resize=1018,1536&quality=75&strip=all";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The Guest Room by Tasha Sylva"/> 
+         <img src={book2} alt="The Manor House by Gilly MacMillan" />
+         <img src={book3} alt="A Likeable Woman by May Cobb" />
       </div>      
    );
 }
