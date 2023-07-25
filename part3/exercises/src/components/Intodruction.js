66fcbf16ce9a86de7d083a296d7a6c17fe41c3a3
Erin.Schneider <erinsuzy@gmail.com>
2023-07-25 10:51:52 -0500

    ordered list change

--- part3/exercises/src/components/Intodruction.js
+++ part3/exercises/src/components/Intodruction.js
@@ -3,9 +3,9 @@ export default function HobbyIntroduction () {
         <div>
             <h2>3 Fun Facts About this Hobby:
                 <ol>
-                    <li>It's mostly free, you just need a smartphone with the app</li>
-                    <li>You can geocache alone or with others</li>
-                    <li>You get to explore places you wouldn't normally go</li>
+                    <li>You can geocache alone or with others.</li>
+                    <li>You get to explore places you wouldn't normally go.</li>
+                    <li>It's mostly free, you just need a smartphone with the app.</li>
                 </ol>
             </h2>
         </div>
