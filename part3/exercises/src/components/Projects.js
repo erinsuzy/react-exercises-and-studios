e823b4aa2fdf7ebe96b232b3644bcaf7e97089f3
Erin.Schneider <erinsuzy@gmail.com>
2023-07-25 12:13:40 -0500

    projects start

---
+++ part3/exercises/src/components/Projects.js
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import './../data.json';
+
+export default function MyProjects () {
+    const [index, setIndex] = useState(0);
+
+    const cacheData = 
+    return();
+}
