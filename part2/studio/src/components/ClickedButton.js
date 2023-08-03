723e03b154d7ff8cd469463ed8d5e0c74866596b
Erin.Schneider <erinsuzy@gmail.com>
2023-08-02 21:06:25 -0500

    rate and buttons

--- part2/studio/src/components/ClickedButton.js
+++ part2/studio/src/components/ClickedButton.js
@@ -1,5 +1,16 @@
+import './styling.css';
+
 function ClickedButton() {
-  return;
+  function handleClick() {
+    alert("This pin has been removed!");
+  }
+  return (
+    <button id="clickedButton" onClick={handleClick}>
+    Saved 
+    </button>
+  );
+  
+  
 }
 
 export default ClickedButton;
