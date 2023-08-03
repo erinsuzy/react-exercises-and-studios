170ce64742f5ee6f4a766e9f6e728f298d2f2bcd
Erin.Schneider <erinsuzy@gmail.com>
2023-08-02 20:40:37 -0500

    app.js

--- part3/exercises/src/App.js
+++ part3/exercises/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import HobbyIntroduction from './components/Intodruction';
+import MyProjects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
       <h1>My Hobby: Geocaching</h1>
       <HobbyIntroduction />
+      <MyProjects />
     </div>
   );
 }
