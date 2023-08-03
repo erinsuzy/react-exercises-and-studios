be00b5f2b3967ed46d08b37a5850b7912491328c
Erin.Schneider <erinsuzy@gmail.com>
2023-08-02 20:38:39 -0500

    projects.js

--- part3/exercises/src/components/Projects.js
+++ part3/exercises/src/components/Projects.js
@@ -1,30 +1,31 @@
-import { useState } from 'react';
 import data from './../data.json';
+import { useState } from 'react';
 
 export default function MyProjects() {
-    const [index, setIndex] = useState(0);
+   const [index, setIndex] = useState(0);
 
-    function handleClick() {
-        if (index < data.projects.length-1)
-        {
-            setIndex(index + 1);
-        }
-        else
-        {
-            setIndex(0);
-        }
-    }
-    
-    let projects = data.projects;
-    let project = projects[index];
+  function handleClick() {
+   if (index < data.projects.length-1)
+   {
+      setIndex(index + 1);
+   }
+   else 
+   {
+      setIndex(0);
+   }
+  }
 
-    return (
-        <div>
-            <button onClick={handleClick}>Next</button>
-            <h2>{project.cacheDifficulty} in {project.park}</h2>
-            <img src={project.photoUrl} alt={project.alt} />
-        </div>
-    );
-
-
+  let projects = data.projects;
+  let project = projects[index];
+  
+  return (
+    <div>
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <h2>{project.canvas} by {project.designer}
+      </h2>
+      <img src={project.photoUrl} alt={project.alt} />
+    </div>
+  );
 }
